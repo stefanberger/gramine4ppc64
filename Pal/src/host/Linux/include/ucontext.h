25bb080754c1e7d9e3b255af447fee0d51de6b7f
Stefan Berger <stefanb@us.ibm.com>
2020-01-27 16:34:26 -0500

    Pal: make it work on ppc64

--- Pal/src/host/Linux/include/ucontext.h
+++ Pal/src/host/Linux/include/ucontext.h
@@ -26,6 +26,8 @@
    included in <signal.h>.  */
 #include <sigcontext.h>
 
+#if defined(__i386__) || defined (__x86_64__)
+
 #if __WORDSIZE == 64
 
 /* Type for general register.  */
@@ -230,5 +232,110 @@ typedef struct ucontext {
 } ucontext_t;
 
 #endif /* __WORDSIZE == 32 */
+
+#elif defined(__powerpc64__)
+
+struct pt_regs {
+	unsigned long gpr[32];
+	unsigned long nip;
+	unsigned long msr;
+	unsigned long orig_gpr3;        /* Used for restarting system calls */
+	unsigned long ctr;
+	unsigned long link;
+	unsigned long xer;
+	unsigned long ccr;
+#ifdef __powerpc64__
+	unsigned long softe;            /* Soft enabled/disabled */
+#else
+	unsigned long mq;               /* 601 only (not used at present) */
+                                        /* Used on APUS to hold IPL value. */
+#endif
+	unsigned long trap;             /* Reason for being here */
+	/* N.B. for critical exceptions on 4xx, the dar and dsisr
+	   fields are overloaded to hold srr0 and srr1. */
+	unsigned long dar;              /* Fault registers */
+	unsigned long dsisr;            /* on 4xx/Book-E used for ESR */
+	unsigned long result;           /* Result of a system call */
+};
+
+#define __ctx(fld) fld
+
+struct __ctx(pt_regs);
+
+/* Number of general registers.  */
+# define __NGREG        48      /* includes r0-r31, nip, msr, lr, etc.   */
+# define __NFPREG       33      /* includes fp0-fp31 &fpscr.  */
+# define __NVRREG       34      /* includes v0-v31, vscr, & vrsave in
+                                   split vectors */
+
+typedef unsigned long gregset_t[__NGREG];
+typedef double fpregset_t[__NFPREG];
+
+/* Container for Altivec/VMX Vector Status and Control Register.  Only 32-bits
+   but can only be copied to/from a 128-bit vector register.  So we allocated
+   a whole quadword speedup save/restore.  */
+typedef struct _libc_vscr
+{
+	unsigned int __pad[3];
+	unsigned int __ctx(vscr_word);
+} vscr_t;
+
+/* Container for Altivec/VMX registers and status.
+   Must to be aligned on a 16-byte boundary. */
+typedef struct _libc_vrstate
+{
+	unsigned int    __ctx(vrregs)[32][4];
+	vscr_t          __ctx(vscr);
+	unsigned int    __ctx(vrsave);
+	unsigned int    __pad[3];
+} vrregset_t  __attribute__((__aligned__(16)));
+
+typedef struct {
+	unsigned long   __glibc_reserved[4];
+	int             __ctx(signal);
+	int             __pad0;
+	unsigned long   __ctx(handler);
+	unsigned long   __ctx(oldmask);
+	struct __ctx(pt_regs)   *__ctx(regs);
+	gregset_t       __ctx(gp_regs);
+	fpregset_t      __ctx(fp_regs);
+/*
+ * To maintain compatibility with current implementations the sigcontext is
+ * extended by appending a pointer (v_regs) to a quadword type (elf_vrreg_t)
+ * followed by an unstructured (vmx_reserve) field of 69 doublewords.  This
+ * allows the array of vector registers to be quadword aligned independent of
+ * the alignment of the containing sigcontext or ucontext. It is the
+ * responsibility of the code setting the sigcontext to set this pointer to
+ * either NULL (if this processor does not support the VMX feature) or the
+ * address of the first quadword within the allocated (vmx_reserve) area.
+ *
+ * The pointer (v_regs) of vector type (elf_vrreg_t) is essentually
+ * an array of 34 quadword entries.  The entries with
+ * indexes 0-31 contain the corresponding vector registers.  The entry with
+ * index 32 contains the vscr as the last word (offset 12) within the
+ * quadword.  This allows the vscr to be stored as either a quadword (since
+ * it must be copied via a vector register to/from storage) or as a word.
+ * The entry with index 33 contains the vrsave as the first word (offset 0)
+ * within the quadword.
+ */
+	vrregset_t      *__ctx(v_regs);
+	long            __ctx(vmx_reserve)[__NVRREG+__NVRREG+1];
+} mcontext_t;
+
+/* Userlevel context.  */
+typedef struct ucontext
+  {
+    unsigned long int __ctx(uc_flags);
+    struct ucontext *uc_link;
+    stack_t uc_stack;
+    __sigset_t    uc_sigmask;
+    mcontext_t  uc_mcontext;  /* last for extensibility */
+  } ucontext_t;
+
+#undef __ctx
+
+#else
+# error Unsupported architecture
+#endif
 
 #endif /* ucontext.h */
