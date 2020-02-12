5d93edb26ac9bc2e261ebca7135616feaf1a3c9c
Stefan Berger <stefanb@us.ibm.com>
2020-02-12 10:44:18 -0500

    Travis: Use Travis for CI/CD
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- python/graminelibos/regression.py
+++ python/graminelibos/regression.py
@@ -1,3 +1,6 @@
+
+
+
 import contextlib
 import logging
 import os
@@ -16,6 +19,7 @@ fspath = getattr(os, 'fspath', str) # pylint: disable=invalid-name
 HAS_SGX = os.environ.get('SGX') == '1'
 ON_X86 = os.uname().machine in ['x86_64']
 USES_MUSL = os.environ.get('GRAMINE_MUSL') == '1'
+ON_TRAVIS = os.getenv('TRAVIS') == 'true'
 
 def expectedFailureIf(predicate):
     if predicate:
