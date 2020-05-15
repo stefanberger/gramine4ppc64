387f6fd85d603565ec11a93f98f9b1f59689bd5f
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [Pal] Make the Pal layer work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- pal/src/host/linux/pal_linux_defs.h
+++ pal/src/host/linux/pal_linux_defs.h
@@ -3,5 +3,14 @@
 /* mmap minimum address cannot start at zero (modern OSes do not allow this) */
 #define MMAP_MIN_ADDR 0x10000
 
+#if defined(__powerpc64__)
+
+#define THREAD_STACK_SIZE (PRESET_PAGESIZE)
+#define ALT_STACK_SIZE    (PRESET_PAGESIZE)
+
+#else
+
 #define THREAD_STACK_SIZE (PRESET_PAGESIZE * 16) /* 64KB user stack */
 #define ALT_STACK_SIZE    (PRESET_PAGESIZE * 16) /* 64KB signal stack */
+
+#endif
