1024c48eb38b8bad777bdd1f060bf19a4dc8b7cb
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [PAL] Use #defines for va_list ap -> va_list VA_AP(ap)
    On powerpc64 the function that is called with a 'va_list ap' parameter
    must either process the 'ap' parameter itself or must pass it to other
    functions as a pointer. So those 2nd level functions need to have a
    parameter 'va_list *ap'. This patch now solves this problem using #define's
    that we apply to those 2nd level functions signature where the parameter
    is now 'va_list VA_AP(ap)'. VA_AP on x86_64 is simply again 'ap' while on
    powerpc64 it would be '*ap'. Since powerpc64 needs to be able to create a
    pointer to pass to the 2nd level functions, we also introduce VA_AP_PARAM(AP)
    that on powerpc64 would be '&(AP)' but on x86_64 can again be 'AP' since no
    pointer is needed.

--- Pal/include/arch/x86_64/pal-arch.h
+++ Pal/include/arch/x86_64/pal-arch.h
@@ -183,4 +183,11 @@ typedef struct PAL_CONTEXT_ PAL_CONTEXT;
 
 #define DEFAULT_OBJECT_EXEC_ADDR ((void*)0x555555554000) /* Linux base location for PIE binaries */
 
+/*
+ * Vararg parameters: On x86_64 a va_list parameter can be passed to any function using
+ *                    'va_list ap'. Therefore VA_AP is 'ap'.
+ */
+#define VA_AP(AP)       (AP)
+#define VA_AP_PARAM(AP) (AP)
+
 #endif /* PAL_ARCH_H */
