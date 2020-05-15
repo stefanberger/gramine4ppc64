a52d440f386f12d67fa2bc4e3407917ff80f865d
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [Pal] Make the Pal layer work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ Pal/src/host/Linux/arch/ppc64/db_arch_misc.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2014 Stony Brook University */
+
+/*
+ * This file contains ppc64-specific functions of the PAL loader.
+ */
+
+#include "api.h"
+#include "cpu.h"
+#include "linux_utils.h"
+#include "pal_linux.h"
+#include "topo_info.h"
