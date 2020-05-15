2ab8fb8c0288759e76005e36b53429de207fd99f
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [Pal] Make the Pal layer work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ common/include/arch/ppc64/cpu.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#ifndef CPU_H
+#define CPU_H
+
+#include <stdnoreturn.h>
+
+#define PAGE_SIZE       (1ul << 16)
+#define PRESET_PAGESIZE PAGE_SIZE
+
+#define CR0_SO		(1 << 28)  /* summary overflow; indicates syscall error */
+
+static inline void cpu_pause(void) {
+}
+
+/* Optimization barrier */
+#define CPU_RELAX() __asm__ __volatile__("or 1,1,1; or 2,2,2" ::: "memory")
+
+#define MB() __asm__ __volatile__("": : :"memory")
+
+static inline noreturn void die_or_inf_loop(void) {
+    do {
+        *(int*)~0 = 0;
+    } while (1);
+}
+
+#endif /* CPU_H */
