a3837b854d71dd6f51144361e36a348a7b903809
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [Pal] Make the Pal layer work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ Pal/include/arch/ppc64/Linux/sigcontext.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#ifndef _LINUX_PPC64_BITS_SIGCONTEXT_H
+#define _LINUX_PPC64_BITS_SIGCONTEXT_H 1
+
+#include <bits/wordsize.h>
+#include <stdint.h>
+
+#endif /* _LINUX_PPC64_BITS_SIGCONTEXT_H */
