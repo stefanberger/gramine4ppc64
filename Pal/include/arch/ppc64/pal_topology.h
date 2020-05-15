69c24b481c58ae3977c66cd56c322633beeceba4
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [Pal] Make the Pal layer work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ Pal/include/arch/ppc64/pal_topology.h
@@ -0,0 +1,88 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2022 Intel Corporation
+ *                    Michał Kowalczyk <mkow@invisiblethingslab.com>
+ * Copyright (C) 2020 IBM Corporation
+ */
+
+#ifndef PAL_TOPOLOGY_H
+#define PAL_TOPOLOGY_H
+
+/* Used to represent plain integers (only numeric values) */
+#define PAL_SYSFS_INT_FILESZ 16
+/* Used to represent buffers having numeric values with text. E.g "1024576K" */
+#define PAL_SYSFS_BUF_FILESZ 64
+/* Used to represent cpumaps like "00000000,ffffffff,00000000,ffffffff" */
+#define PAL_SYSFS_MAP_FILESZ 256
+
+enum {
+    HUGEPAGES_2M = 0,
+    HUGEPAGES_1G,
+    HUGEPAGES_MAX,
+};
+
+struct pal_core_cache_info {
+    char shared_cpu_map[PAL_SYSFS_MAP_FILESZ];
+    char level[PAL_SYSFS_INT_FILESZ];
+    char type[PAL_SYSFS_BUF_FILESZ];
+    char size[PAL_SYSFS_BUF_FILESZ];
+    char coherency_line_size[PAL_SYSFS_INT_FILESZ];
+    char number_of_sets[PAL_SYSFS_INT_FILESZ];
+#if 0
+    char physical_line_partition[PAL_SYSFS_INT_FILESZ]; // on x86_64
+#endif
+};
+
+struct pal_core_topo_info {
+    /* [0] element is uninitialized because core 0 is always online */
+    char is_logical_core_online[PAL_SYSFS_INT_FILESZ];
+    char core_id[PAL_SYSFS_INT_FILESZ];
+    char core_siblings[PAL_SYSFS_MAP_FILESZ];
+    char thread_siblings[PAL_SYSFS_MAP_FILESZ];
+    /* Array of size cache_indices_cnt, owned by this struct */
+    struct pal_core_cache_info* cache_info_arr;
+};
+
+struct pal_numa_hugepage_info {
+    char nr_hugepages[PAL_SYSFS_INT_FILESZ];
+};
+
+struct pal_numa_topo_info {
+    char cpumap[PAL_SYSFS_MAP_FILESZ];
+    char distance[PAL_SYSFS_INT_FILESZ];
+    struct pal_numa_hugepage_info hugepages[HUGEPAGES_MAX];
+};
+
+struct pal_topo_info {
+   /* Number of logical cores available on the host. */
+    size_t online_logical_cores_cnt;
+
+    char online_logical_cores[PAL_SYSFS_BUF_FILESZ];
+
+    /* Array of "logical core -> socket" mappings; has online_logical_cores_cnt elements. */
+    size_t* cpu_to_socket_arr;
+
+    /* Array of logical core topology info, owned by this struct. Has online_logical_cores_cnt
+     * elements. */
+    struct pal_core_topo_info* core_topology_arr;
+
+    /* Max number of logical cores available on the host. */
+    size_t possible_logical_cores_cnt;
+
+    char possible_logical_cores[PAL_SYSFS_BUF_FILESZ];
+
+    /* Number of physical cores in a socket (physical package). */
+    size_t physical_cores_per_socket;
+
+    /* Number of nodes available on the host. */
+    size_t online_nodes_cnt;
+
+    char online_nodes[PAL_SYSFS_BUF_FILESZ];
+
+    /* Array of numa topology info, owned by this struct. Has online_nodes_cnt elements. */
+    struct pal_numa_topo_info* numa_topology_arr;
+
+    /* Number of cache levels (such as L2 or L3) available on the host. */
+    size_t cache_indices_cnt;
+};
+
+#endif /* PAL_TOPOLOGY_H */
--- Pal/src/host/Linux-common/topo_info.c
+++ Pal/src/host/Linux-common/topo_info.c
@@ -167,18 +167,37 @@ static int get_cache_topo_info(size_t cache_indices_cnt, size_t core_idx,
         snprintf(filename, sizeof(filename),
                  "/sys/devices/system/cpu/cpu%zu/cache/index%zu/coherency_line_size", core_idx,
                  cache_idx);
-        READ_FILE_BUFFER(filename, cache_info->coherency_line_size, /*failure_label=*/out_cache);
+
+        READ_FILE_BUFFER(filename, cache_info->coherency_line_size,
+#if defined(__x86_64__)
+                         /*failure_label=*/out_cache);
+#elif defined(__powerpc64__)
+        /* FIXME: not always available */
+                         /*failure label=*/skip1);
+skip1:
+#endif
 
         snprintf(filename, sizeof(filename),
                  "/sys/devices/system/cpu/cpu%zu/cache/index%zu/number_of_sets", core_idx,
                  cache_idx);
-        READ_FILE_BUFFER(filename, cache_info->number_of_sets, /*failure_label=*/out_cache);
+        READ_FILE_BUFFER(filename, cache_info->number_of_sets,
+#if defined(__x86_64__)
+                         /*failure_label=*/out_cache);
+#elif defined(__powerpc64__)
+        /* FIXME: not always available */
+                         /*failure label=*/skip2);
+skip2:
+        (void)cache_idx;
+#endif
 
+#if defined(__x86_64__)
+        /* FIXME: Not available on ppc64 */
         snprintf(filename, sizeof(filename),
                  "/sys/devices/system/cpu/cpu%zu/cache/index%zu/physical_line_partition", core_idx,
                  cache_idx);
         READ_FILE_BUFFER(filename, cache_info->physical_line_partition,
                          /*failure_label=*/out_cache);
+#endif
     }
     *out_cache_info_arr = cache_info_arr;
     return 0;
@@ -253,8 +272,12 @@ static int get_core_topo_info(struct pal_topo_info* topo_info) {
                  "/sys/devices/system/cpu/cpu%zu/topology/physical_package_id", idx);
         ret = get_hw_resource(filename, /*count=*/false);
         if (ret < 0) {
+#if !defined(__powerpc64__)
             log_warning("Cannot read %s", filename);
             goto out_cpu_to_socket;
+#else
+            cpu_to_socket_arr[idx] = idx;
+#endif
         }
         cpu_to_socket_arr[idx] = (size_t)ret;
     }
@@ -265,7 +288,14 @@ static int get_core_topo_info(struct pal_topo_info* topo_info) {
     if (!core_topology_arr)
         return -ENOMEM;
 
-    for (size_t idx = 0; idx < online_logical_cores_cnt; idx++) {
+    size_t step = 1;
+#if defined(__powerpc64__)
+    // FIXME: too many processors; ltp tests are failing due to timeouts
+    step = 8;
+    memset(core_topology_arr, 0, sizeof(struct pal_core_topo_info));
+    memcpy(core_topology_arr[0].is_logical_core_online, "1\n", 2);
+#endif
+    for (size_t idx = 0; idx < online_logical_cores_cnt; idx+=step) {
         /* cpu0 is always online and thus the "online" file is not present. */
         if (idx != 0) {
             snprintf(filename, sizeof(filename), "/sys/devices/system/cpu/cpu%zu/online", idx);
@@ -289,13 +319,17 @@ static int get_core_topo_info(struct pal_topo_info* topo_info) {
         ret = get_cache_topo_info(cache_indices_cnt, idx, &core_topology_arr[idx].cache_info_arr);
         if (ret < 0)
             goto out_topology;
+        for (size_t j = idx + 1; j < idx + step && j < online_logical_cores_cnt; j++)
+            core_topology_arr[j] = core_topology_arr[idx];
     }
     topo_info->core_topology_arr = core_topology_arr;
     return 0;
 
 out_topology:
     free(core_topology_arr);
+#if !defined(__powerpc64__)
 out_cpu_to_socket:
+#endif
     free(cpu_to_socket_arr);
 out:
     return ret;
@@ -319,11 +353,18 @@ static int get_numa_topo_info(struct pal_topo_info* topo_info) {
         return -ENOMEM;
 
     char filename[128];
-    for (size_t idx = 0; idx < online_nodes_cnt; idx++) {
-        snprintf(filename, sizeof(filename), "/sys/devices/system/node/node%zu/cpumap", idx);
-        READ_FILE_BUFFER(filename, numa_topology_arr[idx].cpumap, /*failure_label=*/out_topology);
-
-        snprintf(filename, sizeof(filename), "/sys/devices/system/node/node%zu/distance", idx);
+    size_t sidx = 0;
+    for (size_t idx = 0; idx < online_nodes_cnt; idx++, sidx++) {
+        while (1) {
+            snprintf(filename, sizeof(filename), "/sys/devices/system/node/node%zu/cpumap", sidx);
+            READ_FILE_BUFFER(filename, numa_topology_arr[idx].cpumap, /*failure_label=*/next_sidx);
+            break;
+next_sidx:
+            sidx++;
+            if (sidx == 2048)
+                goto out_topology;
+        }
+        snprintf(filename, sizeof(filename), "/sys/devices/system/node/node%zu/distance", sidx);
         READ_FILE_BUFFER(filename, numa_topology_arr[idx].distance, /*failure_label=*/out_topology);
 
         /* Since our /sys fs doesn't support writes, set persistent hugepages to their default value
