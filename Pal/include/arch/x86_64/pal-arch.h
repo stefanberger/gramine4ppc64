c4872823db139bf4594ff8d7dd321c62d661124b
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    [Pal] Make the Pal layer work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- Pal/include/arch/x86_64/pal-arch.h
+++ Pal/include/arch/x86_64/pal-arch.h
@@ -55,6 +55,10 @@ static inline void pal_tcb_arch_set_stack_canary(PAL_TCB* tcb, uint64_t canary) 
 static_assert(offsetof(PAL_TCB, stack_protector_canary) == 0x8,
               "unexpected offset of stack_protector_canary in PAL_TCB struct");
 
+static inline void pal_tcb_arch_init(PAL_TCB* tcb __attribute__((unused))) {
+    /* nothing to do */
+}
+
 #include "pal_host-arch.h"
 
 static inline PAL_TCB* pal_get_tcb(void) {
