50fb629ff1ee4d1b0b848258549b0c5e47fcd825
Stefan Berger <stefanb@us.ibm.com>
2020-05-15 15:22:56 +0000

    Pal: make it work on ppc64

--- Pal/src/host/Linux/db_main.c
+++ Pal/src/host/Linux/db_main.c
@@ -173,7 +173,9 @@ noreturn void pal_linux_main(void* initial_rsp, void* fini_callback) {
     __UNUSED(fini_callback);  // TODO: We should call `fini_callback` at the end.
 
     uint64_t start_time = _DkSystemTimeQueryEarly();
+#if !defined(__powerpc64__)
     g_pal_state.start_time = start_time;
+#endif
 
     int ret;
     int argc;
@@ -198,6 +200,9 @@ noreturn void pal_linux_main(void* initial_rsp, void* fini_callback) {
     ELF_DYNAMIC_RELOCATE(&g_pal_map);
 
     g_linux_state.host_environ = envp;
+#if defined(__powerpc64__)
+    g_pal_state.start_time = start_time;
+#endif
 
     init_slab_mgr(g_page_size);
 
@@ -225,7 +230,7 @@ noreturn void pal_linux_main(void* initial_rsp, void* fini_callback) {
     tcb->alt_stack   = alt_stack; // Stack bottom
     tcb->callback    = NULL;
     tcb->param       = NULL;
-
+    pal_tcb_arch_init(&tcb->common);
     ret = pal_thread_init(tcb);
     if (ret < 0)
         INIT_FAIL(unix_to_pal_error(-ret), "pal_thread_init() failed");
