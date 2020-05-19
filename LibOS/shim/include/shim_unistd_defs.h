c48dbf508f0da658051466d4965e9dae7fcd36a8
Stefan Berger <stefanb@linux.ibm.com>
2020-05-19 11:55:08 -0400

    LibOS: Make LibOS work on PPC64

--- LibOS/shim/include/shim_unistd_defs.h
+++ LibOS/shim/include/shim_unistd_defs.h
@@ -3,6 +3,8 @@
 
 #if defined(__i386__) || defined(__x86_64__)
 #define LIBOS_SYSCALL_BASE  340
+#elif defined(__powerpc64__)
+#define LIBOS_SYSCALL_BASE  500
 #endif
 
 #define LIBOS_SYSCALL_BOUND (LIBOS_SYSCALL_BASE + 1 + 4) /* 4 custom syscalls */
