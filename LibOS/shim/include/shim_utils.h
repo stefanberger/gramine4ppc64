79f0e810698b72ac855c5db04e45914ca1a2cd1c
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on PPC64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- LibOS/shim/include/shim_utils.h
+++ LibOS/shim/include/shim_utils.h
@@ -173,5 +173,8 @@ int write_exact(PAL_HANDLE handle, void* buf, size_t size);
 static inline uint64_t timespec_to_us(const struct __kernel_timespec* ts) {
     return ts->tv_sec * TIME_US_IN_S + ts->tv_nsec / TIME_NS_IN_US;
 }
+static inline uint64_t timespec64_to_us(const struct timespec64* ts) {
+    return ts->tv_sec * TIME_US_IN_S + (uint64_t)ts->tv_nsec / TIME_NS_IN_US;
+}
 
 #endif /* _SHIM_UTILS_H */
