febfdb829972e82fbb7310f3f9bc525e2f4920dd
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- libos/test/regression/asm/x86_64/test_x86_64.py
+++ libos/test/regression/asm/x86_64/test_x86_64.py
@@ -3,12 +3,14 @@
 import unittest
 
 from graminelibos.regression import (
+    ON_X86,
     USES_MUSL,
     RegressionTestCase,
 )
 
 # These tests don't require libc, so it doesn't make sense to run them
 # twice (once with glibc and once with musl) as the result is not dependable on libc.
+@unittest.skipUnless(ON_X86, 'x86-specific')
 @unittest.skipIf(USES_MUSL, 'Assembly tests are disabled with musl')
 class TC_00_X86_64(RegressionTestCase):
     # The iret emulation is used only on PAL/Linux-SGX.
