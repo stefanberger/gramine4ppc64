7126834df93d8f66144f4f03ed2512a4b6479a27
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    LibOS: Make LibOS work on PPC64

---
+++ LibOS/shim/include/arch/ppc64/shim_vdso-arch.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#ifndef _SHIM_VDSO_ARCH_H_
+#define _SHIM_VDSO_ARCH_H_
+
+#define LINUX_VDSO_FILENAME "linux-vdso64.so.1"
+
+#endif /* _SHIM_VDSO_ARCH_H_ */
