a1c7213d4e5ff63775ebb421b1ecb96445aeef83
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- libos/include/linux_abi/process.h
+++ libos/include/linux_abi/process.h
@@ -8,6 +8,8 @@
 /* Types and structures used by various Linux ABIs (e.g. syscalls). */
 /* These need to be binary-identical with the ones used by Linux. */
 
+#if defined(__x86_64__)
 #include <asm/prctl.h>
+#endif
 #include <linux/sched.h> // for CLONE_* stuff
 #include <linux/wait.h>
