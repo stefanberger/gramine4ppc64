9f4d8e19a329eb6f5e005b886bfd68e988f6888d
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on ppc64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ libos/src/vdso/arch/ppc64/vdso.c
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2018 Intel Corporation
+ *                    Isaku Yamahata <isaku.yamahata at gmail.com>
+ *                                   <isaku.yamahata at intel.com>
+ * Copyright (C) 2020 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
+ *
+ * Copyright (C) 2022 IBM Corporation
+ *
+ */
+
+#include <asm/unistd.h>
+
+#include "vdso.h"
+#include "gramine_entry_api.h"
+
+int c__kernel_gettimeofday(struct timeval* tv, struct timezone* tz) {
+    return gramine_syscall(__NR_gettimeofday, (long)tv, (long)tz, 0, 0, 0, 0);
+}
+
+int c__kernel_clock_gettime(clockid_t clock_id, struct timespec* tp) {
+    return gramine_syscall(__NR_clock_gettime, (long)clock_id, (long)tp, 0, 0, 0, 0);
+}
+
+int c__kernel_clock_getres(clockid_t clock_id, struct timespec* tp) {
+    return gramine_syscall(__NR_clock_getres, (long)clock_id, (long)tp, 0, 0, 0, 0);
+}
+
+time_t c__kernel_time(time_t* t) {
+    return gramine_syscall(__NR_time, (long)t, 0, 0, 0, 0, 0);
+}
+
+long c__kernel_getcpu(unsigned* cpu, struct getcpu_cache* unused) {
+    return gramine_syscall(__NR_getcpu, (long)cpu, (long)unused, 0, 0, 0, 0);
+}
