687480fb628876cc7bb93047c582a845158470c2
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on PPC64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- LibOS/shim/test/regression/test_libos.py
+++ LibOS/shim/test/regression/test_libos.py
@@ -11,6 +11,8 @@ from graminelibos.regression import (
     HAS_SGX,
     ON_X86,
     USES_MUSL,
+    ON_PPC,
+    ON_TRAVIS,
     RegressionTestCase,
 )
 
@@ -748,6 +750,11 @@ class TC_30_Syscall(RegressionTestCase):
         stdout, _ = self.run_binary(['kill_all'])
         self.assertIn('TEST OK', stdout)
 
+    @unittest.skipUnless(ON_PPC, "ppc64-specific")
+    def test_096_sighandler_contextswitch(self):
+        stdout, _ = self.run_binary(['sighandler_contextswitch'])
+        self.assertIn('TEST OK', stdout)
+
     def test_100_get_set_groups(self):
         stdout, _ = self.run_binary(['groups'])
         self.assertIn('child OK', stdout)
@@ -866,6 +873,7 @@ class TC_40_FileSystem(RegressionTestCase):
         self.assertGreater(n, 0, "no information about CPU cache found")
         return n
 
+    @unittest.skipIf((ON_TRAVIS & ON_PPC) | ON_PPC, "necessary files not available on Travis")
     def test_040_sysfs(self):
         num_cpus = os.cpu_count()
         num_cache_levels = self.get_num_cache_levels()
@@ -952,6 +960,7 @@ class TC_40_FileSystem(RegressionTestCase):
             self.assertIn('Permission denied', stdout)
 
 
+@unittest.skipIf(ON_PPC, "not supported on PowerPC64")
 class TC_50_GDB(RegressionTestCase):
     def setUp(self):
         if not self.has_debug():
@@ -1104,7 +1113,7 @@ class TC_80_Socket(RegressionTestCase):
         if os.path.exists("u"):
             os.remove("u")
 
-        stdout, _ = self.run_binary(['unix'])
+        stdout, _ = self.run_binary(['unix'], timeout=20)
         self.assertIn('Data: This is packet 0', stdout)
         self.assertIn('Data: This is packet 1', stdout)
         self.assertIn('Data: This is packet 2', stdout)
