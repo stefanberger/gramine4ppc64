f1dd7dd5362af4fe767f5e5c7f07b2ac890751c7
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on PPC64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ LibOS/shim/include/arch/ppc64/shim_table-arch.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#ifndef SHIM_TABLE_PPC64_H
+#define SHIM_TABLE_PPC64_H
+
+noreturn void shim_table_dispatch(PAL_CONTEXT *context);
+
+#endif /* SHIM_TABLE_PPC64_H */
