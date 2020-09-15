a2548311e2e481149884f7df315e339582568bb4
Stefan Berger <stefanb@linux.ibm.com>
2020-09-15 09:23:03 -0400

    [LibOS] Make LibOS work on PPC64
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ LibOS/shim/include/arch/ppc64/shim_types-arch.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2020 IBM Corporation */
+
+#ifndef _SHIM_TYPES_ARCH_H_
+#define _SHIM_TYPES_ARCH_H_
+
+#include <assert.h>
+#include <stdint.h>
+
+#include "shim_tcb-arch.h"
+
+/* asm/signal.h */
+#define NUM_SIGS            64
+#define NUM_KNOWN_SIGS      32
+
+typedef struct {
+    unsigned long __val[NUM_SIGS / (8 * sizeof(unsigned long))];
+} __sigset_t;
+
+#endif /* _SHIM_TYPES_ARCH_H_ */
