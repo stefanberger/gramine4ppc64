9bf67566b41b3512de43ab291cb5b0481f4b5ddd
Stefan Berger <stefanb@linux.ibm.com>
2021-10-27 20:45:14 +0000

    WIP: PAL: Rework ELF parsing/RTLD code FOR PPC64

--- Pal/src/host/Linux/db_main.c
+++ Pal/src/host/Linux/db_main.c
@@ -189,6 +189,7 @@ noreturn void pal_linux_main(void* initial_rsp, void* fini_callback) {
     }
 
     /* relocate PAL and populate g_pal_map */
+    g_pal_map.in_memory = 1;
     ret = setup_pal_binary(&g_pal_map);
     if (ret < 0)
         INIT_FAIL(-ret, "Relocation of the PAL binary failed");
@@ -286,9 +287,11 @@ noreturn void pal_linux_main(void* initial_rsp, void* fini_callback) {
         INIT_FAIL(unix_to_pal_error(-ret), "pal_thread_init() failed");
 
     if (g_sysinfo_ehdr) {
+#if defined(__x86_64__)
         ret = setup_vdso(g_sysinfo_ehdr);
         if (ret < 0)
             INIT_FAIL(-ret, "Setup of VDSO failed");
+#endif
     }
 
     uintptr_t vdso_start = 0;
