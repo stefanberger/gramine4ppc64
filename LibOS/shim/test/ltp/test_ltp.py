38852bfaaff51bb2802d6711e21a689eb554db4e
Stefan Berger <stefanb@linux.ibm.com>
2022-01-07 21:08:06 +0000

    ltp: Enable skipping of tests by architecture

--- LibOS/shim/test/ltp/test_ltp.py
+++ LibOS/shim/test/ltp/test_ltp.py
@@ -60,6 +60,8 @@ class Config:
     """
 
     def __init__(self, config_paths):
+        machine = os.uname().machine
+
         self.cfg = configparser.ConfigParser(
             converters={
                 'path': pathlib.Path,
@@ -82,8 +84,11 @@ class Config:
                         raise ValueError(
                             'wildcard sections like {!r} can only contain "skip", not {!r}'.format(
                                 name, key))
-                if section.get('skip'):
-                    self.skip_patterns.append(name)
+                skips = section.get('skip')
+                if skips:
+                    skip_list = [ e.strip() for e in skips.split(',') ]
+                    if 'yes' in skip_list or machine in skip_list:
+                        self.skip_patterns.append(name)
 
     def get(self, tag):
         """Find a section for given tag.
