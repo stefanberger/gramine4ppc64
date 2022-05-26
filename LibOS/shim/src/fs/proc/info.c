058afca17831beb6d74da899a7967ddbe7f9bdaa
Stefan Berger <stefanb@linux.ibm.com>
2022-05-26 02:51:17 +0000

    [LibOS] Move procfs cpuinfo display code into functions
    Move the code for generating the output of /proc/cpuinfo into functions
    that each architecture has to implement.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- LibOS/shim/src/fs/proc/info.c
+++ LibOS/shim/src/fs/proc/info.c
@@ -8,6 +8,7 @@
  */
 
 #include "shim_fs_pseudo.h"
+#include "shim_proc.h"
 
 int proc_meminfo_load(struct shim_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
@@ -93,7 +94,7 @@ static void* realloc_size(void* ptr, size_t old_size, size_t new_size) {
     return tmp;
 }
 
-static int print_to_str(char** str, size_t off, size_t* size, const char* fmt, ...) {
+int print_to_str(char** str, size_t off, size_t* size, const char* fmt, ...) {
     int ret;
     va_list ap;
 
@@ -119,6 +120,38 @@ retry:
     return ret;
 }
 
+int proc_cpuinfo_load(struct shim_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+
+    int ret;
+    size_t size = 0;
+    size_t max = 128;
+    char* str = malloc(max);
+    if (!str)
+        return -ENOMEM;
+
+    const struct pal_topo_info* topo = &g_pal_public_state->topo_info;
+    const struct pal_cpu_info* cpu = &g_pal_public_state->cpu_info;
+    for (size_t i = 0; i < topo->threads_cnt; i++) {
+        struct pal_cpu_thread_info* thread = &topo->threads[i];
+        if (!thread->is_online) {
+            /* Offline cores are skipped in cpuinfo, with gaps in numbering. */
+            continue;
+        }
+        ret = proc_cpuinfo_display_cpu(&str, &size, &max, topo, cpu, i, thread);
+        if (ret)
+            return ret;
+    }
+
+    ret = proc_cpuinfo_display_tail(&str, &size, &max, cpu);
+    if (ret)
+        return ret;
+
+    *out_data = str;
+    *out_size = size;
+    return 0;
+}
+
 #define ADD_INFO(fmt, ...)                                            \
     do {                                                              \
         int ret = print_to_str(&str, size, &max, fmt, ##__VA_ARGS__); \
@@ -128,74 +161,6 @@ retry:
         }                                                             \
         size += ret;                                                  \
     } while (0)
-
-int proc_cpuinfo_load(struct shim_dentry* dent, char** out_data, size_t* out_size) {
-    __UNUSED(dent);
-
-    size_t size = 0;
-    size_t max = 128;
-    char* str = malloc(max);
-    if (!str)
-        return -ENOMEM;
-
-    const struct pal_topo_info* topo = &g_pal_public_state->topo_info;
-#if defined(__x86_64__)
-    const struct pal_cpu_info* cpu = &g_pal_public_state->cpu_info;
-#endif
-    for (size_t i = 0; i < topo->threads_cnt; i++) {
-        struct pal_cpu_thread_info* thread = &topo->threads[i];
-        if (!thread->is_online) {
-            /* Offline cores are skipped in cpuinfo, with gaps in numbering. */
-            continue;
-        }
-#if defined(__x86_64__)
-        struct pal_cpu_core_info* core = &topo->cores[thread->core_id];
-        /* Below strings must match exactly the strings retrieved from /proc/cpuinfo
-         * (see Linux's arch/x86/kernel/cpu/proc.c) */
-        ADD_INFO("processor\t: %lu\n",   i);
-        ADD_INFO("vendor_id\t: %s\n",    cpu->cpu_vendor);
-        ADD_INFO("cpu family\t: %lu\n",  cpu->cpu_family);
-        ADD_INFO("model\t\t: %lu\n",     cpu->cpu_model);
-        ADD_INFO("model name\t: %s\n",   cpu->cpu_brand);
-        ADD_INFO("stepping\t: %lu\n",    cpu->cpu_stepping);
-        ADD_INFO("physical id\t: %zu\n", core->socket_id);
-
-        /* Linux keeps this numbering socket-local, but we can use a different one, and it's
-         * documented as "hardware platform's identifier (rather than the kernel's)" anyways. */
-        ADD_INFO("core id\t\t: %lu\n",   thread->core_id);
-
-        size_t cores_in_socket = 0;
-        for (size_t j = 0; j < topo->cores_cnt; j++) { // slow, but shouldn't matter
-            if (topo->cores[j].socket_id == core->socket_id)
-                cores_in_socket++;
-        }
-        ADD_INFO("cpu cores\t: %zu\n", cores_in_socket);
-        double bogomips = cpu->cpu_bogomips;
-        // Apparently Gramine snprintf cannot into floats.
-        ADD_INFO("bogomips\t: %lu.%02lu\n", (unsigned long)bogomips,
-                 (unsigned long)(bogomips * 100.0 + 0.5) % 100);
-#elif defined(__powerpc64__)
-        ADD_INFO("processor\t: %lu\n", i);
-        ADD_INFO("cpu\t\t: %s\n", g_pal_public_state->cpu_info.cpu);
-        ADD_INFO("clock\t\t: %s\n", g_pal_public_state->cpu_info.clock);
-        ADD_INFO("revision\t: %s\n", g_pal_public_state->cpu_info.revision);
-#endif
-        ADD_INFO("\n");
-    }
-
-#if defined(__powerpc64__)
-    ADD_INFO("timebase\t: %s\n", g_pal_public_state->cpu_info.timebase);
-    ADD_INFO("platform\t: %s\n", g_pal_public_state->cpu_info.platform);
-    ADD_INFO("model\t\t: %s\n", g_pal_public_state->cpu_info.model);
-    ADD_INFO("machine\t\t: %s\n", g_pal_public_state->cpu_info.machine);
-    ADD_INFO("firmware\t: %s\n", g_pal_public_state->cpu_info.firmware);
-    ADD_INFO("MMU\t\t: %s\n", g_pal_public_state->cpu_info.mmu);
-#endif
-
-    *out_data = str;
-    *out_size = size;
-    return 0;
-}
 
 int proc_stat_load(struct shim_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
