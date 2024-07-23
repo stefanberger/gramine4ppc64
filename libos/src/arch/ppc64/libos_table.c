d0357341eb33d6de23096fe0301b7b6116ef0668
Stefan Berger <stefanb@linux.ibm.com>
2024-07-23 14:54:09 +0000

    make it work on ppc64

--- libos/src/arch/ppc64/libos_table.c
+++ libos/src/arch/ppc64/libos_table.c
@@ -467,6 +467,9 @@ libos_syscall_t libos_syscall_table[LIBOS_SYSCALL_BOUND] = {
     (libos_syscall_t)0,
 };
 
+/* by default, all syscalls have `is_mocked = false` and `return_value = 0` */
+struct libos_mock_syscall libos_mock_syscall_table[LIBOS_SYSCALL_BOUND] = { 0 };
+
 noreturn void libos_syscall_table_dispatch(PAL_CONTEXT* context) {
     /* after a restart_syscall we need to adjust nip to be able to resume properly */
     context->gpregs.nip = context->gpregs.link;
